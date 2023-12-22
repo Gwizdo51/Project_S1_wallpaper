98e3689578d0100d72cbe180c2f181f3fa44a5af
orane meow <tigrou.thethug@gmail.com>
2023-12-22 09:56:51 +0100

    ajouté interface colle

---
+++ logiciel/interface.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "types.h"
+
+int interface_colle(float volume_pots){
+
+    char caractere_entre;
+    BOOL caractere_valide;
+
+    printf("4) CONDITIONNEMENT COLLE \n");
+    printf("Est-ce que la colle est conditionnée ? [O]ui/ [N]on")
+
+    caractere_valide = FAUX;
+
+    while (caractere_valide == FAUX){
+        scanf("%c", &caractere_entre);
+        if(caractere_entre == 'o' || caractere_entre == 'n'){
+            caractere_valide = VRAI;
+        }   
+    }
+
+    if (caractere_entre == 'o'){
+        printf("Volume de colle par pots : ");
+        scanf("%f", &volume_pots);}
+    else {volume_pots = 0;
+     }
+}

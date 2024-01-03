4f0c83947e65846525a4a1ca32daefe7c8b8210e
Gwizdo51 <arthur.clement.51@gmail.com>
2024-01-03 16:05:57 +0100

    les arguments des fonctions sont des pointeurs

--- logiciel/main.c
+++ logiciel/main.c
@@ -10,20 +10,20 @@ int main() {
     char *nom_fichier_resultat;
 
     // saisie des dimensions des murs
-    interface_rouleaux(liste_rouleaux);
-    interface_murs(liste_series_murs, liste_rouleaux);
-    interface_colle(volume_pots);
+    interface_rouleaux(&liste_rouleaux);
+    interface_murs(&liste_series_murs, &liste_rouleaux);
+    interface_colle(&volume_pots);
 
     // calcul de la quantité de colle et du nombre de rouleaux
-    calcul_les(liste_series_murs, liste_rouleaux);
-    calcul_pans_coupes(liste_series_murs);
-    calcul_decoupage_les(liste_series_murs, liste_rouleaux);
-    calcul_obstacle(liste_series_murs, quantite_colle, volume_pots, nombre_pots);
-    calcul_assemblage_les(liste_series_murs, liste_rouleaux);
-    calcul_tapissage(liste_series_murs, liste_rouleaux);
-    calcul_colle(liste_series_murs, quantite_colle, volume_pots, nombre_pots);
+    calcul_les(&liste_series_murs, &liste_rouleaux);
+    calcul_pans_coupes(&liste_series_murs);
+    calcul_decoupage_les(&liste_series_murs, &liste_rouleaux);
+    calcul_obstacle(&liste_series_murs, &quantite_colle, &volume_pots, &nombre_pots);
+    calcul_assemblage_les(&liste_series_murs, &liste_rouleaux);
+    calcul_tapissage(&liste_series_murs, &liste_rouleaux);
+    calcul_colle(&liste_series_murs, &quantite_colle, &volume_pots, &nombre_pots);
 
     // affichage et enregistrement du résultat
-    enregistrer_resultat(liste_rouleaux, quantite_colle, nombre_pots, nom_fichier_resultat);
-    afficher_resultat(liste_rouleaux, quantite_colle, nombre_pots, nom_fichier_resultat);
+    enregistrer_resultat(&liste_rouleaux, &quantite_colle, &nombre_pots, &nom_fichier_resultat);
+    afficher_resultat(&liste_rouleaux, &quantite_colle, &nombre_pots, &nom_fichier_resultat);
 }

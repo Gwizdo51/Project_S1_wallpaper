650a00d6b5203b04df662ba7eda4cd8b5d2759ba
orane meow <tigrou.thethug@gmail.com>
2024-01-03 15:53:54 +0100

    correction

--- logiciel/interface.h
+++ logiciel/interface.h
@@ -1,7 +1,7 @@
 #ifndef _HEADER_INTERFACE_COLLE
 #define _HEADER_INTERFACE_COLLE
 
-int interface_colle(float volume_pots);
-int interface_rouleaux(LL_ROULEAU ROULEAU);
+void interface_colle(float *volume_pots);
+void interface_rouleaux(LL_ROULEAU *ROULEAU);
 
 #endif

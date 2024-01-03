f3c92cf95128f9a1bd574149986faa75fc907aca
orane meow <tigrou.thethug@gmail.com>
2024-01-03 17:20:52 +0100

    calcul colle

--- logiciel/calcul.c
+++ logiciel/calcul.c
@@ -3,17 +3,30 @@
 #include "types.h"
 #include "calcul.h"
 
-void calcul_colle( LL_SERIE_MURS_NODE liste_series_murs, float *volume_pots, float *quantité_colle, int nombre_pots){
+void calcul_colle(LL_SERIE_MURS_NODE liste_series_murs, float *volume_pots, float *quantite_colle, int nombre_pots)
+{
     int indice_mur, indice_obstacle, indice_serie_mur;
     float surface_totale_mur;
     mur *mur_actuel;
 
     surface_totale_mur = 0;
-    for(indice_serie_mur = 0; indice_serie_mur < (liste_series_murs) - 1 ; indice_serie_mur++){
-        for(indice_mur = 0; indice_mur < longueur(liste_series_murs, indice_serie_mur).liste_mur - 1 ; indice_mur++){
+    for (indice_serie_mur = 0; indice_serie_mur < (liste_series_murs)-1; indice_serie_mur++)
+    {
+        for (indice_mur = 0; indice_mur < longueur(liste_series_murs, indice_serie_mur).liste_mur - 1; indice_mur++)
+        {
             *mur_actuel = liste_series_murs(indice_serie_mur).liste_mur(indice_mur);
             surface_totale_mur = surface_totale_mur + (*mur_actuel).hauteur * (*mur_actuel).largeur;
-            
+            for (indice_obstacle = 0; indice_obstacle < longueur(mur_actuel.liste_obstacles) - 1; indice_obstacle++)
+            {
+                surface_totale_mur = surface_totale_mur - mur_actuel.liste_obstacles(indice_obstacle).hauteur * mur_actuel.liste_obstacles(indice_obstacle).largeur;
+            }
+            surface_totale_mur = surface_totale_mur - mur_actuel.hauteur_pan_gauche * mur_actuel.largeur_pan_gauche / 2;
+            surface_totale_mur = surface_totale_mur - mur_actuel.hauteur_pan_droite * mur_actuel.largeur_pan_droite / 2;
         }
+    }
+    *quantite_colle = surface_totale_mur * 0.002;
+    if (*volume_pots != 0)
+    {
+        nombre_pots = ceiling(*quantite_colle / *volume_pots);
     }
 }

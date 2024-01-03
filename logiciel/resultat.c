0bdfdaaa0cb85112bcac543e9f81c94696997308
orane meow <tigrou.thethug@gmail.com>
2024-01-03 15:34:55 +0100

    interface_rouleaux

--- logiciel/resultat.c
+++ logiciel/resultat.c
@@ -3,24 +3,24 @@
 #include "types.h"
 #include "resultat.h"
 
-int resultat(float quantite_colle, int nombre_pots, char* nom_fichier, LL_ROULEAU liste_rouleaux)
+int resultat(float quantite_colle, int nombre_pots, char *nom_fichier, LL_ROULEAU liste_rouleaux)
 {
 
-printf("4) RESULTAT \n");
-printf("Nombre de rouleaux : \n");
+    printf("4) RESULTAT \n");
+    printf("Nombre de rouleaux : \n");
 
-for (int i = 0; i < longueur(liste_rouleaux) - 1; i++){
-    // printf("PAPIER PEINT #", "%i", ":", obtenir (liste_rouleaux i.quantité_rouleaux));
-    printf("PAPIER PEINT #", "%i", ":", obtenir(liste_rouleaux, i).quantité_rouleaux);
+    for (int i = 0; i < longueur(liste_rouleaux) - 1; i++)
+    {
+        // printf("PAPIER PEINT #", "%i", ":", obtenir (liste_rouleaux i.quantité_rouleaux));
+        printf("PAPIER PEINT #", "%i", ":", obtenir(liste_rouleaux, i).quantité_rouleaux);
+    }
 
-}
+    printf("Volume colle : ", quantite_colle);
 
-printf("Volume colle : ", quantite_colle); 
+    if (nombre_pots != 0)
+    {
+        printf("Nombre de pots de colle : ", nombre_pots);
+    }
 
-if (nombre_pots != 0){
-    printf("Nombre de pots de colle : ", nombre_pots);  
-}
-
-printf("Le resultat est enregistré dans ", nom_fichier);
-
+    printf("Le resultat est enregistré dans ", nom_fichier);
 }

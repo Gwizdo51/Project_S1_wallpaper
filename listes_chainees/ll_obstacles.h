bd70a8cf5e60c59525c7303f01fd44892f48bb74
orane meow <tigrou.thethug@gmail.com>
2024-01-06 14:42:18 +0100

    correction ll_obstacles

--- listes_chainees/ll_obstacles.h
+++ listes_chainees/ll_obstacles.h
@@ -4,8 +4,8 @@
 
 LL_OBSTACLE llo_create();
 int llo_length(LL_OBSTACLE *linked_list);
-void llo_append(LL_OBSTACLE *linked_list, float new_data);
-float *llo_get(LL_OBSTACLE *linked_list, int index);
+void llo_append(LL_OBSTACLE *linked_list, OBSTACLE new_data);
+OBSTACLE *llo_get(LL_OBSTACLE *linked_list, int index);
 void llo_remove(LL_OBSTACLE *linked_list, int index);
 void llo_free(LL_OBSTACLE *linked_list);
 

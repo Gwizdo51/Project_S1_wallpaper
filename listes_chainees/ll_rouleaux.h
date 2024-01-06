2044ec6d42ae62d00ee261454f50b5d40694d260
orane meow <tigrou.thethug@gmail.com>
2024-01-06 14:43:34 +0100

    correction ll_ROULEAUX

--- listes_chainees/ll_rouleaux.h
+++ listes_chainees/ll_rouleaux.h
@@ -4,8 +4,8 @@
 
 LL_ROULEAU llr_create();
 int llr_length(LL_ROULEAU *linked_list);
-void llr_append(LL_ROULEAU *linked_list, float new_data);
-float *llr_get(LL_ROULEAU *linked_list, int index);
+void llr_append(LL_ROULEAU *linked_list, ROULEAU new_data);
+ROULEAU *llr_get(LL_ROULEAU *linked_list, int index);
 void llr_remove(LL_ROULEAU *linked_list, int index);
 void llr_free(LL_ROULEAU *linked_list);
 

ca9fb65504a74cca526e714ad013cfea29f5403b
Gwizdo51 <arthur.clement.51@gmail.com>
2024-01-06 19:04:45 +0100

    correction + format interface

--- logiciel/interface.c
+++ logiciel/interface.c
@@ -1,84 +1,70 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "types.h"
 #include "interface.h"
 
-void interface_colle(float *volume_pots);
-{
 
-    char caractere_entre;
-    BOOL caractere_valide;
-
-    printf("4) CONDITIONNEMENT COLLE \n");
-    printf("Est-ce que la colle est conditionnée ? [O]ui/ [N]on");
-
-    caractere_valide = FALSE;
-
-    while (!caractere_valide)
-    {
-        scanf("%c", &caractere_entre);
-        if (caractere_entre == 'o' || caractere_entre == 'n')
-        {
-            caractere_valide = TRUE;
-        }
-    }
-
-    if (caractere_entre == 'o')
-    {
-        printf("Volume de colle par pots : ");
-        scanf("%f", volume_pots);
-    }
-    else
-    {
-        *volume_pots = 0;
-    }
-}
-
-void interface_rouleaux(LL_ROULEAU *ROULEAU);
-{
+void interface_rouleaux(LL_ROULEAU *liste_rouleaux) {
+    // variables
     char caractere_entre;
     BOOL caractere_valide;
     int indice_rouleau;
-    int nombre_types;
+    int nombre_types_rouleaux;
+    ROULEAU nouveau_rouleau;
 
-    printf("1) INVENTAIRE PAPIER PEINT");
+    printf("1) INVENTAIRE PAPIER PEINT\n");
     printf("Nombre de type de papier peints differents : ");
-    scanf("%d", &nombre_types);
-    for (indice_rouleau = 0; indice_rouleau < llr_length(nombre_types) - 1; indice_rouleau++)
-    {
-        LL_ROULEAU liste_rouleau = llr_create();
-        ROULEAU nouveau_rouleau;
-        // rouleau <- NOUVEAU rouleau
-        printf("Dimension rouleaux papier peints : ");
+    scanf("%d", &nombre_types_rouleaux);
+    for (indice_rouleau = 0; indice_rouleau < nombre_types_rouleaux; indice_rouleau++) {
+        printf("Type de papier n°%d\n", indice_rouleau);
+        printf("Dimension rouleaux papier peints :\n");
         printf("Largeur : ");
-        scanf("%d", &rouleau.largeur);
+        scanf("%f", &nouveau_rouleau.largeur);
         printf("Longueur : ");
-        scanf("%d", &rouleau.longueur);
-        printf("Motif ? [O]ui/ [N]on");
+        scanf("%f", &nouveau_rouleau.longueur);
+        printf("Le papier peint a-t-il un motif ? [O]ui/[N]on\n");
         caractere_valide = FALSE;
-        while(caractere_valide == FALSE)
-        {
-            scanf("%c", &caractere_entre);
-            if (caractere_entre == 'o' || caractere_entre == 'n')
-            {
+        while(caractere_valide == FALSE) {
+            // scanf("%c", &caractere_entre);
+            caractere_entre = _getch();
+            if (caractere_entre == 'o' || caractere_entre == 'n') {
                 caractere_valide = TRUE;
             }
         }
-        if (caractere_entre == 'o')
-        {
-            printf("Longueur motif : ");
-            scanf("%s", &rouleau.longueur_motif);
+        if (caractere_entre == 'o') {
+            printf("Entrez la longueur motif : ");
+            scanf("%f", &nouveau_rouleau.longueur_motif);
         }
-        else
-        {
-            rouleau.longueur_motif = 0;
+        else {
+            nouveau_rouleau.longueur_motif = 0.;
         }
-        rouleau.longueur_restante = 0;
-        rouleau.quantité = 0;
-        LL_ROULEAU liste_chutes = llr_create();
-        // rouleau.liste_chutes <- créer()
-        llr_append(&liste_rouleau, nouveau_rouleau);        
-        // ajouter(liste_rouleau, rouleau)
+        // initialisation des attributs du nouveau rouleau
+        nouveau_rouleau.longueur_restante = 0;
+        nouveau_rouleau.quantite = 0;
+        nouveau_rouleau.liste_chutes = llf_create();
+        llr_append(liste_rouleaux, nouveau_rouleau);
+        printf("\n");
+    }
+}
+
+
+void interface_colle(float *volume_pots) {
+    // variables
+    char caractere_entre;
+    BOOL caractere_valide;
+
+    printf("4) CONDITIONNEMENT COLLE\n");
+    printf("Est-ce que la colle est conditionnée ? [O]ui/ [N]on\n");
+    caractere_valide = FALSE;
+    while (!caractere_valide) {
+        // scanf("%c", &caractere_entre);
+        caractere_entre = _getch();
+        if (caractere_entre == 'o' || caractere_entre == 'n') {
+            caractere_valide = TRUE;
+        }
+    }
+    if (caractere_entre == 'o') {
+        printf("Entrez le volume de colle par pots : ");
+        scanf("%f", volume_pots);
+    }
+    else {
+        *volume_pots = 0;
     }
 }
--- logiciel/interface.h
+++ logiciel/interface.h
@@ -1,9 +1,10 @@
 #ifndef _HEADER_INTERFACE_COLLE
 #define _HEADER_INTERFACE_COLLE
+
+#include "types.h"
 #include "..\listes_chainees\ll_rouleaux.h"
 
+void interface_rouleaux(LL_ROULEAU *liste_rouleaux);
 void interface_colle(float *volume_pots);
-void interface_rouleaux(LL_ROULEAU *ROULEAU);
-
 
 #endif

818f7c077dc14ffd8b88831c446c24c11b95fa4f
Gwizdo51 <arthur.clement.51@gmail.com>
2024-01-07 18:20:33 +0100

    tests + bugfix calcul_pans_coupes

--- logiciel/calcul.c
+++ logiciel/calcul.c
@@ -70,24 +70,30 @@ void calcul_pans_coupes(LL_SERIE_MURS *liste_series_murs) {
     MUR *mur_actuel_pointeur;
     LE *le_actuel_pointeur;
 
+    // printf("debut calcul_pans_coupes\n");
     // pour chaque série de murs ...
     for (indice_serie_murs = 0; indice_serie_murs < llsm_length(liste_series_murs); indice_serie_murs++) {
+        // printf("serie de murs actuelle : %d\n", indice_serie_murs);
         // pour chaque mur de la série ...
         for (indice_mur = 0; indice_mur < llm_length(&llsm_get(liste_series_murs, indice_serie_murs)->liste_murs); indice_mur++) {
             mur_actuel_pointeur = llm_get(&llsm_get(liste_series_murs, indice_serie_murs)->liste_murs, indice_mur);
+            // printf("mur actuel : %d\n", indice_mur);
             // pour chaque lé du mur ...
             for (indice_le = 0; indice_le < lll_length(&mur_actuel_pointeur->liste_les); indice_le++) {
                 le_actuel_pointeur = lll_get(&mur_actuel_pointeur->liste_les, indice_le);
+                // printf("le actuel : %d\n", indice_le);
                 // si le lé est complètement en dessous du pan coupé gauche ...
                 if (le_actuel_pointeur->X + le_actuel_pointeur->largeur < mur_actuel_pointeur->largeur_pan_gauche) {
+                    // printf("le le est completement en dessous du pan coupe gauche\n");
                     // on donne au lé la hauteur de son bord droit
                     le_actuel_pointeur->hauteur = mur_actuel_pointeur->hauteur_pan_gauche +
                                                   (mur_actuel_pointeur->hauteur - mur_actuel_pointeur->hauteur_pan_gauche) *
                                                   (le_actuel_pointeur->X + le_actuel_pointeur->largeur) /
-                                                  mur_actuel_pointeur->hauteur_pan_gauche;
+                                                  mur_actuel_pointeur->largeur_pan_gauche;
                 }
                 // si le lé est complètement en dessous du pan coupé droit ...
-                else if (le_actuel_pointeur->X > mur_actuel_pointeur->largeur - mur_actuel_pointeur->hauteur_pan_droit) {
+                else if (le_actuel_pointeur->X > mur_actuel_pointeur->largeur - mur_actuel_pointeur->largeur_pan_droit) {
+                    // printf("le le est completement en dessous du pan coupe droit\n");
                     // on donne au lé la hauteur de son bord gauche
                     le_actuel_pointeur->hauteur = mur_actuel_pointeur->hauteur_pan_droit +
                                                   (mur_actuel_pointeur->hauteur - mur_actuel_pointeur->hauteur_pan_droit) *
--- tests/tests.c
+++ tests/tests.c
@@ -48,13 +48,30 @@ void test_interface_rouleaux() {
     }
 }
 
+/*
+pour tester:
+    1 piece, 3 murs
+    mur 1 : largeur 10, hauteur 4
+    pan coupé gauche 2 large 1 haut, pan coupé droit 2 large 1.5 haut
+    1 obstacle 1, 1, 1, 1
+    pp 0
 
+    mur 2: largeur 15, hauteur 4.5
+    pas de pans coupés
+    pas d'obstacles
+    pp 1
+
+    mur 3: largeur 15, hauteur 3.75
+    pas de pas coupés
+    1 obstacle 1.5, 2, 0.5, 0.5
+    pp 1, adjacent
+*/
 void test_interface_murs() {
     int indice_serie_murs, indice_mur, indice_obstacle;
     LL_ROULEAU liste_rouleaux = llr_create();
     LL_SERIE_MURS liste_series_murs = llsm_create();
     ROULEAU rouleau;
-    SERIE_MURS *serie_mur_pointeur;
+    SERIE_MURS *series_mur_pointeur;
     MUR *mur_pointeur;
     OBSTACLE *obstacle_pointeur;
     // on ajoute des rouleaux à la liste
@@ -69,12 +86,12 @@ void test_interface_murs() {
     // check des séries de murs créées
     printf("nombre de series de murs creees : %d\n\n", llsm_length(&liste_series_murs));
     for (indice_serie_murs = 0; indice_serie_murs < llsm_length(&liste_series_murs); indice_serie_murs++) {
-        serie_mur_pointeur = llsm_get(&liste_series_murs, indice_serie_murs);
+        series_mur_pointeur = llsm_get(&liste_series_murs, indice_serie_murs);
         printf("serie de murs n°%d\n", indice_serie_murs);
-        printf("\ttype de papier peint : %d\n", serie_mur_pointeur->type_papier_peint);
-        printf("\tnombre de murs dans la serie : %d\n", llm_length(&serie_mur_pointeur->liste_murs));
-        for (indice_mur = 0; indice_mur < llm_length(&serie_mur_pointeur->liste_murs); indice_mur++) {
-            mur_pointeur = llm_get(&serie_mur_pointeur->liste_murs, indice_mur);
+        printf("\ttype de papier peint : %d\n", series_mur_pointeur->type_papier_peint);
+        printf("\tnombre de murs dans la serie : %d\n", llm_length(&series_mur_pointeur->liste_murs));
+        for (indice_mur = 0; indice_mur < llm_length(&series_mur_pointeur->liste_murs); indice_mur++) {
+            mur_pointeur = llm_get(&series_mur_pointeur->liste_murs, indice_mur);
             printf("\tmur n°%d\n", indice_mur);
             printf("\t\tlargeur : %.2f\n", mur_pointeur->largeur);
             printf("\t\thauteur : %.2f\n", mur_pointeur->hauteur);
@@ -85,8 +102,8 @@ void test_interface_murs() {
             printf("\t\tlongueur(liste_les) : %d\n", lll_length(&mur_pointeur->liste_les));
             printf("\t\tlongueur(liste_obstacles) : %d\n", llo_length(&mur_pointeur->liste_obstacles));
             for (indice_obstacle = 0; indice_obstacle < llo_length(&mur_pointeur->liste_obstacles); indice_obstacle++) {
-                printf("\t\tobstacle n°%d\n", indice_obstacle);
                 obstacle_pointeur = llo_get(&mur_pointeur->liste_obstacles, indice_obstacle);
+                printf("\t\tobstacle n°%d\n", indice_obstacle);
                 printf("\t\t\tX : %.2f\n", obstacle_pointeur->X);
                 printf("\t\t\tY : %.2f\n", obstacle_pointeur->Y);
                 printf("\t\t\tlargeur : %.2f\n", obstacle_pointeur->largeur);
@@ -105,17 +122,201 @@ void test_interface_colle() {
 }
 
 
-// void test_calcul_les() {
+void test_calcul_les() {
+    LL_ROULEAU liste_rouleaux = llr_create();
+    LL_SERIE_MURS liste_series_murs = llsm_create();
+    int indice_serie_murs, indice_mur, indice_obstacle, indice_le;
+    ROULEAU rouleau;
+    SERIE_MURS serie_murs, *serie_murs_pointeur;
+    MUR mur, *mur_pointeur;
+    OBSTACLE *obstacle_pointeur;
+    LE *le_pointeur;
+    // on ajoute des rouleaux à la liste
+    rouleau.longueur = 10;
+    rouleau.largeur = 1;
+    rouleau.longueur_motif = 0;
+    llr_append(&liste_rouleaux, rouleau);
+    rouleau.largeur = 2;
+    rouleau.longueur_motif = 5;
+    llr_append(&liste_rouleaux, rouleau);
+    // on ajoute des séries de murs
+    serie_murs.liste_murs = llm_create();
+    serie_murs.type_papier_peint = 0;
+    mur.largeur = 5;
+    mur.hauteur = 4;
+    mur.largeur_pan_droit = 0;
+    mur.hauteur_pan_droit = 0;
+    mur.largeur_pan_gauche = 0;
+    mur.hauteur_pan_gauche = 0;
+    mur.liste_les = lll_create();
+    mur.liste_obstacles = llo_create();
+    llm_append(&serie_murs.liste_murs, mur);
+    llsm_append(&liste_series_murs, serie_murs);
+    serie_murs.liste_murs = llm_create();
+    serie_murs.type_papier_peint = 1;
+    mur.largeur = 5;
+    mur.hauteur = 4;
+    mur.largeur_pan_droit = 0;
+    mur.hauteur_pan_droit = 0;
+    mur.largeur_pan_gauche = 0;
+    mur.hauteur_pan_gauche = 0;
+    mur.liste_les = lll_create();
+    mur.liste_obstacles = llo_create();
+    llm_append(&serie_murs.liste_murs, mur);
+    mur.largeur = 5;
+    mur.hauteur = 4;
+    mur.largeur_pan_droit = 0;
+    mur.hauteur_pan_droit = 0;
+    mur.largeur_pan_gauche = 0;
+    mur.hauteur_pan_gauche = 0;
+    mur.liste_les = lll_create();
+    mur.liste_obstacles = llo_create();
+    llm_append(&serie_murs.liste_murs, mur);
+    llsm_append(&liste_series_murs, serie_murs);
+    // appel calcul_les
+    calcul_les(&liste_series_murs, &liste_rouleaux);
+    // check des lés créés
+    printf("nombre de series de murs creees : %d\n\n", llsm_length(&liste_series_murs));
+    for (indice_serie_murs = 0; indice_serie_murs < llsm_length(&liste_series_murs); indice_serie_murs++) {
+        serie_murs_pointeur = llsm_get(&liste_series_murs, indice_serie_murs);
+        printf("serie de murs n°%d\n", indice_serie_murs);
+        printf("\ttype de papier peint : %d\n", serie_murs_pointeur->type_papier_peint);
+        printf("\tnombre de murs dans la serie : %d\n", llm_length(&serie_murs_pointeur->liste_murs));
+        for (indice_mur = 0; indice_mur < llm_length(&serie_murs_pointeur->liste_murs); indice_mur++) {
+            mur_pointeur = llm_get(&serie_murs_pointeur->liste_murs, indice_mur);
+            printf("\tmur n°%d\n", indice_mur);
+            printf("\t\tlargeur : %.2f\n", mur_pointeur->largeur);
+            printf("\t\thauteur : %.2f\n", mur_pointeur->hauteur);
+            printf("\t\thauteur_pan_droit : %.2f\n", mur_pointeur->hauteur_pan_droit);
+            printf("\t\tlargeur_pan_droit : %.2f\n", mur_pointeur->largeur_pan_droit);
+            printf("\t\thauteur_pan_gauche : %.2f\n", mur_pointeur->hauteur_pan_gauche);
+            printf("\t\tlargeur_pan_gauche : %.2f\n", mur_pointeur->largeur_pan_gauche);
+            printf("\t\tlongueur(liste_obstacles) : %d\n", llo_length(&mur_pointeur->liste_obstacles));
+            for (indice_obstacle = 0; indice_obstacle < llo_length(&mur_pointeur->liste_obstacles); indice_obstacle++) {
+                obstacle_pointeur = llo_get(&mur_pointeur->liste_obstacles, indice_obstacle);
+                printf("\t\tobstacle n°%d\n", indice_obstacle);
+                printf("\t\t\tX : %.2f\n", obstacle_pointeur->X);
+                printf("\t\t\tY : %.2f\n", obstacle_pointeur->Y);
+                printf("\t\t\tlargeur : %.2f\n", obstacle_pointeur->largeur);
+                printf("\t\t\thauteur : %.2f\n", obstacle_pointeur->hauteur);
+            }
+            printf("\t\tlongueur(liste_les) : %d\n", lll_length(&mur_pointeur->liste_les));
+            for (indice_le = 0; indice_le < lll_length(&mur_pointeur->liste_les); indice_le++) {
+                le_pointeur = lll_get(&mur_pointeur->liste_les, indice_le);
+                printf("\t\tle n°%d\n", indice_le);
+                printf("\t\t\tX : %.2f\n", le_pointeur->X);
+                printf("\t\t\tY : %.2f\n", le_pointeur->Y);
+                printf("\t\t\tlargeur : %.2f\n", le_pointeur->largeur);
+                printf("\t\t\thauteur : %.2f\n", le_pointeur->hauteur);
+            }
+        }
+        printf("\n");
+    }
+}
 
-// }
+
+void test_calcul() {
+    LL_ROULEAU liste_rouleaux = llr_create();
+    LL_SERIE_MURS liste_series_murs = llsm_create();
+    int indice_serie_murs, indice_mur, indice_obstacle, indice_le;
+    ROULEAU rouleau;
+    SERIE_MURS serie_murs, *serie_murs_pointeur;
+    MUR mur, *mur_pointeur;
+    OBSTACLE *obstacle_pointeur;
+    LE *le_pointeur;
+    // on ajoute des rouleaux à la liste
+    rouleau.longueur = 10;
+    rouleau.largeur = 1;
+    rouleau.longueur_motif = 0;
+    llr_append(&liste_rouleaux, rouleau);
+    rouleau.largeur = 2;
+    rouleau.longueur_motif = 5;
+    llr_append(&liste_rouleaux, rouleau);
+    // on ajoute des séries de murs
+    serie_murs.liste_murs = llm_create();
+    serie_murs.type_papier_peint = 0;
+    mur.largeur = 5;
+    mur.hauteur = 4;
+    mur.largeur_pan_droit = 2;
+    mur.hauteur_pan_droit = 3;
+    mur.largeur_pan_gauche = 3;
+    mur.hauteur_pan_gauche = 3;
+    mur.liste_les = lll_create();
+    mur.liste_obstacles = llo_create();
+    llm_append(&serie_murs.liste_murs, mur);
+    llsm_append(&liste_series_murs, serie_murs);
+    serie_murs.liste_murs = llm_create();
+    serie_murs.type_papier_peint = 1;
+    mur.largeur = 5;
+    mur.hauteur = 4;
+    mur.largeur_pan_droit = 0;
+    mur.hauteur_pan_droit = 0;
+    mur.largeur_pan_gauche = 0;
+    mur.hauteur_pan_gauche = 0;
+    mur.liste_les = lll_create();
+    mur.liste_obstacles = llo_create();
+    llm_append(&serie_murs.liste_murs, mur);
+    mur.largeur = 5;
+    mur.hauteur = 4;
+    mur.largeur_pan_droit = 0;
+    mur.hauteur_pan_droit = 0;
+    mur.largeur_pan_gauche = 0;
+    mur.hauteur_pan_gauche = 0;
+    mur.liste_les = lll_create();
+    mur.liste_obstacles = llo_create();
+    llm_append(&serie_murs.liste_murs, mur);
+    llsm_append(&liste_series_murs, serie_murs);
+    // appel calcul_les
+    calcul_les(&liste_series_murs, &liste_rouleaux);
+    // appel calcul_pans_coupés
+    calcul_pans_coupes(&liste_series_murs);
+    // check des lés créés
+    printf("nombre de series de murs creees : %d\n\n", llsm_length(&liste_series_murs));
+    for (indice_serie_murs = 0; indice_serie_murs < llsm_length(&liste_series_murs); indice_serie_murs++) {
+        serie_murs_pointeur = llsm_get(&liste_series_murs, indice_serie_murs);
+        printf("serie de murs n°%d\n", indice_serie_murs);
+        printf("\ttype de papier peint : %d\n", serie_murs_pointeur->type_papier_peint);
+        printf("\tnombre de murs dans la serie : %d\n", llm_length(&serie_murs_pointeur->liste_murs));
+        for (indice_mur = 0; indice_mur < llm_length(&serie_murs_pointeur->liste_murs); indice_mur++) {
+            mur_pointeur = llm_get(&serie_murs_pointeur->liste_murs, indice_mur);
+            printf("\tmur n°%d\n", indice_mur);
+            printf("\t\tlargeur : %.2f\n", mur_pointeur->largeur);
+            printf("\t\thauteur : %.2f\n", mur_pointeur->hauteur);
+            printf("\t\thauteur_pan_droit : %.2f\n", mur_pointeur->hauteur_pan_droit);
+            printf("\t\tlargeur_pan_droit : %.2f\n", mur_pointeur->largeur_pan_droit);
+            printf("\t\thauteur_pan_gauche : %.2f\n", mur_pointeur->hauteur_pan_gauche);
+            printf("\t\tlargeur_pan_gauche : %.2f\n", mur_pointeur->largeur_pan_gauche);
+            printf("\t\tlongueur(liste_obstacles) : %d\n", llo_length(&mur_pointeur->liste_obstacles));
+            for (indice_obstacle = 0; indice_obstacle < llo_length(&mur_pointeur->liste_obstacles); indice_obstacle++) {
+                obstacle_pointeur = llo_get(&mur_pointeur->liste_obstacles, indice_obstacle);
+                printf("\t\tobstacle n°%d\n", indice_obstacle);
+                printf("\t\t\tX : %.2f\n", obstacle_pointeur->X);
+                printf("\t\t\tY : %.2f\n", obstacle_pointeur->Y);
+                printf("\t\t\tlargeur : %.2f\n", obstacle_pointeur->largeur);
+                printf("\t\t\thauteur : %.2f\n", obstacle_pointeur->hauteur);
+            }
+            printf("\t\tlongueur(liste_les) : %d\n", lll_length(&mur_pointeur->liste_les));
+            for (indice_le = 0; indice_le < lll_length(&mur_pointeur->liste_les); indice_le++) {
+                le_pointeur = lll_get(&mur_pointeur->liste_les, indice_le);
+                printf("\t\tle n°%d\n", indice_le);
+                printf("\t\t\tX : %.2f\n", le_pointeur->X);
+                printf("\t\t\tY : %.2f\n", le_pointeur->Y);
+                printf("\t\t\tlargeur : %.2f\n", le_pointeur->largeur);
+                printf("\t\t\thauteur : %.2f\n", le_pointeur->hauteur);
+            }
+        }
+        printf("\n");
+    }
+}
 
 
 int main() {
     // test_float_equality();
     // test_interface_rouleaux();
     // test_interface_murs();
-    test_interface_colle();
+    // test_interface_colle();
     // test_calcul_les();
+    test_calcul();
 
     return EXIT_SUCCESS;
 }

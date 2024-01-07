32b2fb7c799e67243e27d471edb13a4a017c4523
Gwizdo51 <arthur.clement.51@gmail.com>
2024-01-07 23:03:04 +0100

    bugfix string buffer size

--- logiciel/resultat.c
+++ logiciel/resultat.c
@@ -13,13 +13,13 @@ void enregistrer_resultat(LL_ROULEAU *liste_rouleaux, float *quantite_colle, int
     // on place dans time_now_pointer la date et l'heure actuelles
     time(&timestamp_now);
     time_now_pointer = localtime(&timestamp_now);
-    strftime(nom_fichier_resultat, 100, "%Y%m%d-%H%M%S - resultats.txt", time_now_pointer);
+    strftime(nom_fichier_resultat, STRING_BUFFER_MAX_SIZE, "%Y%m%d-%H%M%S - resultats.txt", time_now_pointer);
 
     // on crée le fichier et on l'ouvre en écriture
     stream = fopen(nom_fichier_resultat, "w");
 
     // on ajoute la date et l'heure de l'obtention du résultat
-    strftime(date_heure_humain, 100, "%c", time_now_pointer);
+    strftime(date_heure_humain, STRING_BUFFER_MAX_SIZE, "%c", time_now_pointer);
     // on ajoute les résultats
     fprintf(stream, "date et heure : %s\n\n", date_heure_humain);
     fprintf(stream, "Matiere premiere a commander :\n\n");
